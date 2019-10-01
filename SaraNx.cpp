e5201a7f7952cf70fb28d8432a50a6aafb88d1a4
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-01 17:22:21 +0100

    Add empty sara lib files

---
+++ SaraNx.cpp
@@ -0,0 +1,11 @@
+/**
+  * @file    SaraNx.cpp
+  * @version 1.0.0
+  * @author  
+  * @brief   C++ file of the NB-IOT driver module
+  */
+
+/** Includes
+ */
+#include "SaraNx.h"
+
---
+++ SaraNx.h
@@ -0,0 +1,15 @@
+/**
+  * @file    SaraNx.h
+  * @version 1.0.0
+  * @author  
+  * @brief   Header file of the  NB-IOT driver module
+  */
+
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes 
+ */
+#include <mbed.h>
+
